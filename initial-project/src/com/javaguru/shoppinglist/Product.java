14b6adfa527ecdd737a67b30b9f09d29ea8336ea
lvvorovi <vitalijs.bmi@gmail.com>
2020-05-18 22:05:16 +0300

    Task 1 finished @override toString at Product class

--- initial-project/src/com/javaguru/shoppinglist/Product.java
+++ initial-project/src/com/javaguru/shoppinglist/Product.java
@@ -7,6 +7,9 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private int discount;
+    private String description;
 
     public Long getId() {
         return id;
@@ -21,6 +24,7 @@ public class Product {
     }
 
     public void setName(String name) {
+        if (name.length() >= 3 && name.length() <= 32 )
         this.name = name;
     }
 
@@ -29,7 +33,46 @@ public class Product {
     }
 
     public void setPrice(BigDecimal price) {
-        this.price = price;
+        if ((price.compareTo(new BigDecimal(0))) > 0) {
+            this.price = price;
+        }
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        if (discount <= 100) {
+            this.discount = discount;
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
