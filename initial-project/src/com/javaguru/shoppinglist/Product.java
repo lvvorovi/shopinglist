9214c7955c2d65b20719d82382f8fbc1346ba207
lvvorovi <65552616+lvvorovi@users.noreply.github.com>
2020-05-19 15:57:11 +0300

    Task1 (#1)
    * Task 1 finished. + overrides toString, hashCode, equals.
    
    * Task 1 finished. + overrides toString, hashCode, equals.
    
    * Task 1 finished. + overrides toString, hashCode, equals.

--- initial-project/src/com/javaguru/shoppinglist/Product.java
+++ initial-project/src/com/javaguru/shoppinglist/Product.java
@@ -1,13 +1,16 @@
 package com.javaguru.shoppinglist;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
     private Long id;
     private String name;
     private BigDecimal price;
-
+    private String category;
+    private int discount;
+    private String description;
 
     public Long getId() {
         return id;
@@ -22,7 +25,9 @@ public class Product {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name.length() >= 3 && name.length() <= 32){
+            this.name = name;
+        }
     }
 
     public BigDecimal getPrice() {
@@ -30,6 +35,64 @@ public class Product {
     }
 
     public void setPrice(BigDecimal price) {
+        if ((price.compareTo(new BigDecimal(0))) > 0 ){
             this.price = price;
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        if (discount <= 100){
+            this.discount = discount;
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return getDiscount() == product.getDiscount() &&
+                getId().equals(product.getId()) &&
+                getName().equals(product.getName()) &&
+                getPrice().equals(product.getPrice()) &&
+                Objects.equals(getCategory(), product.getCategory()) &&
+                Objects.equals(getDescription(), product.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPrice(), getCategory(), getDiscount(), getDescription());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", discount=" + discount +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
